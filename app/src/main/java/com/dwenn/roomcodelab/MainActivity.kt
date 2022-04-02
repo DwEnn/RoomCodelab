aef7a58cb37bb2c0004d8de346e82b3869c52327
DwEnn <prgohn@gmail.com>
2022-04-03 00:20:41 +0900

    8. Room 데이터베이스 추가

--- app/src/main/java/com/dwenn/roomcodelab/MainActivity.kt
+++ app/src/main/java/com/dwenn/roomcodelab/MainActivity.kt
@@ -2,10 +2,13 @@ package com.dwenn.roomcodelab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dwenn.roomcodelab.data.WordRoomDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WordRoomDatabase.getDatabase(this)
     }
 }
--- app/src/main/java/com/dwenn/roomcodelab/data/WordDAO.kt
+++ app/src/main/java/com/dwenn/roomcodelab/data/WordDAO.kt
@@ -1,10 +1,12 @@
 package com.dwenn.roomcodelab.data
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface WordDAO {
     @Query("SELECT * FROM word_table ORDER BY word ASC")
     fun getAlphabetizedWords(): Flow<List<Word>>
